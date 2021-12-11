9e1d00ad255531b1f9febca9e0894ffa04cdca8d
martinmakejev <martin.makejev@tptlive.ee>
2021-12-11 11:48:01 +0200

    added photos and changed margins and paddings

--- src/App.vue
+++ src/App.vue
@@ -13,34 +13,43 @@
             <div class="content">
                  <div class="row">
                   <div class="column">
+                    <img class="img" src="https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/2022-bmw-m5-cs-109-1611684117.jpg?crop=0.708xw:0.798xh;0.0865xw,0.115xh&resize=640:*" width="100%">
                     <div class="card">BMW M5</div>
                   </div>
                   <div class="column">
+                    <img class="img" src="https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/2022-bmw-m5-cs-109-1611684117.jpg?crop=0.708xw:0.798xh;0.0865xw,0.115xh&resize=640:*" width="100%">
                     <div class="card">530i</div>
                   </div>
                   <div class="column">
+                    <img class="img" src="https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/2022-bmw-m5-cs-109-1611684117.jpg?crop=0.708xw:0.798xh;0.0865xw,0.115xh&resize=640:*" width="100%">
                     <div class="card">Lexus is450</div>
                   </div>
 
                   <div class="row">
                   <div class="column">
+                    <img class="img" src="https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/2022-bmw-m5-cs-109-1611684117.jpg?crop=0.708xw:0.798xh;0.0865xw,0.115xh&resize=640:*" width="100%">
                     <div class="card">BMW M5</div>
                   </div>
                   <div class="column">
+                    <img class="img" src="https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/2022-bmw-m5-cs-109-1611684117.jpg?crop=0.708xw:0.798xh;0.0865xw,0.115xh&resize=640:*" width="100%">
                     <div class="card">530i</div>
                   </div>
                   <div class="column">
+                    <img class="img" src="https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/2022-bmw-m5-cs-109-1611684117.jpg?crop=0.708xw:0.798xh;0.0865xw,0.115xh&resize=640:*" width="100%">
                     <div class="card">Lexus is450</div>
                   </div>
 
                   <div class="row">
                   <div class="column">
+                    <img class="img" src="https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/2022-bmw-m5-cs-109-1611684117.jpg?crop=0.708xw:0.798xh;0.0865xw,0.115xh&resize=640:*" width="100%">
                     <div class="card">BMW M5</div>
                   </div>
                   <div class="column">
+                    <img class="img" src="https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/2022-bmw-m5-cs-109-1611684117.jpg?crop=0.708xw:0.798xh;0.0865xw,0.115xh&resize=640:*" width="100%">
                     <div class="card">530i</div>
                   </div>
                   <div class="column">
+                    <img class="img" src="https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/2022-bmw-m5-cs-109-1611684117.jpg?crop=0.708xw:0.798xh;0.0865xw,0.115xh&resize=640:*" width="100%">
                     <div class="card">Lexus is450</div>
                   </div>
 
@@ -97,7 +106,7 @@ li a:hover {
 .margin {
   border: 10px solid #009900;
   text-align: center;
-  padding: 50px;
+  padding: 150px;
   
 }
       
@@ -108,7 +117,8 @@ li a:hover {
    
 .column {
   float: left;
-  width: 33.33%;
+  width: 32%;
+  height: 32%;
   padding: 0 10px;
 }
 
@@ -121,9 +131,21 @@ li a:hover {
 
 .card {
   box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2); /* this adds the "card" effect */
-  padding: 16px;
+  padding: 10px;
   text-align: center;
   background-color: #f1f1f1;
 }
+ .column {
+   margin-top: 1rem;
+   margin-bottom: 1rem;
+   margin-left: 1rem;
+   background-color: white;
+   border-radius: 3%;
+ }
+
+ .img {
+   margin-top: 1rem;
+   margin-bottom: 1rem;
+ }
 
 </style>
